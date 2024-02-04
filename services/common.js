ff73bfec729862f87dbbfa92e38e8358bd551f53
Samarth <samarthdevlibtech@gmail.com>
2024-02-04 17:11:00 +0530

    first commit

--- services/common.js
+++ services/common.js
@@ -12,12 +12,8 @@ exports.cookieExtractor = function(req) {
   if (req && req.cookies) {
       token = req.cookies['jwt'];
   }
-  // token ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YWUxMGVjMjRkNDM5YjRiMGQ5OGI5NyIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzA1OTA2NDEyfQ.JqoLb-2He7-5KIhXPpboVkZBqxfbirNg2OncurRvXIE";
+  // token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YmY1NDM1Y2I2NGUwYjRjNGJlZTNlYSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzA3MDM3NzQ5fQ.E1qo_5I4E0tnhkv7IAv16RBqUDgQqLHqQFxfhPKuf1M";
 
-  // token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YWUxODk5OTc0Njg2ODk1MGE3YWY3NSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzA1OTA4Mzc3fQ.2u00Uv_pEJY1vp9Go-vKf-M-h_1CwaKHQpTkAU52low";
-  // ajay 
-  // token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YmQyMDRhMTgyN2IyMjVkMjYxOTgyMCIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzA2ODkzMzg3fQ.Y6dcfDTrzPySajdVoqYN6ZsHlxEJs0CoJPZA3B8H0Yg";
-  // token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YjI2ZjY1NWU0YmE4MGE4MTE3NjRhMSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzA2MTkyNzQxfQ.akpgk3eTPM0k4ejEibpNj43t0Y35MihVOng5Aiw_c0Q";
 
   
   return token;
